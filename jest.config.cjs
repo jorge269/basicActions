dc926fdd636d663ff2c5beb8fbd96d426b6348cc
Camilo Pinzón <115189338+kamilo0210@users.noreply.github.com>
2025-10-27 23:50:49 +0000

    Fixes

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,6 +2,17 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["<rootDir>/students/**/tests/**/*.test.js"],
   collectCoverageFrom: ["students/**/src/**/*.js"],
-  coverageDirectory: 'coverage',
-  verbose: true
+  coverageDirectory: "coverage",
+  verbose: true,
+  collectCoverage: true,
+
+  // Exige al menos 80 % de cobertura global
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
